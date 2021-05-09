c52fbd535106070274aa6de6270efe4c45518bcb
Vijayakumar Ravi <vijayravi2998@gmail.com>
2021-05-09 14:48:23 +0530

    redefined arch script

--- Arch-Scripts/arch.sh
+++ Arch-Scripts/arch.sh
@@ -5,7 +5,7 @@
 #DO NOT RUN THIS YOURSELF because Step 1 is it reformatting /dev/sda WITHOUT confirmation,
 #which means RIP in peace qq your data unless you've already backed up all of your drive.
 
-timedatectl set-ntp true
+timedatectl set-ntp true ;
 
 cat <<EOF | fdisk /dev/sda
 o
@@ -44,33 +44,24 @@ EOF
 partprobe
 
 
-mkfs.ext4 /dev/sdb1
-mkfs.ext4 /dev/sda3
-mkfs.ext4 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mount /dev/sda3 /mnt
-mkdir -p /mnt/boot
-mount /dev/sda1 /mnt/boot
-mkdir -p /mnt/home
-mount /dev/sdb1 /mnt/home
+mkfs.ext4 /dev/sdb1 && mkfs.ext4 /dev/sda3 && mkfs.ext4 /dev/sda1 && mkswap /dev/sda2 ;
+swapon /dev/sda2 ;
+mount /dev/sda3 /mnt  && mkdir -p /mnt/boot && mount /dev/sda1 /mnt/boot && mkdir -p /mnt/home && mount /dev/sdb1 /mnt/home ;
 
-lsblk
-sleep 20
+lsblk ;
+sleep 20 ;
 
-pacman -Sy --noconfirm archlinux-keyring
+pacman -Sy --noconfirm archlinux-keyring ;
 
-pacstrap /mnt base base-devel linux linux-lts linux-headers linux-firmware xorg nvidia nvidia-utils gdm gnome git neovim curl telegram-desktop thunderbird gnome-tweaks conky htop neofetch npm python-pip tmux gawk cowsay fortune-mod go grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font cups reflector papirus-icon-theme polkit udisks2 pulseaudio-bluetooth
+pacstrap /mnt base base-devel linux linux-lts linux-headers linux-firmware xorg nvidia nvidia-utils gdm gnome git neovim curl telegram-desktop thunderbird gnome-tweaks conky htop neofetch npm python-pip tmux gawk cowsay fortune-mod go grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font cups reflector papirus-icon-theme polkit udisks2 pulseaudio-bluetooth && genfstab -U /mnt >> /mnt/etc/fstab ;
 
-genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab ;
+sleep 20 ;
 
-cat /mnt/etc/fstab
-sleep 20
+echo "archlinux" >> /mnt/etc/hostname ;
+echo "127.0.0.1 localhost" >> /mnt/etc/hosts ;
+echo "::1       localhost" >> /mnt/etc/hosts ;
+echo "127.0.1.1 archlinux.localdomain archlinux" >> /mnt/etc/hosts ;
 
-echo "archlinux" >> /mnt/etc/hostname
-echo "127.0.0.1 localhost" >> /mnt/etc/hosts
-echo "::1       localhost" >> /mnt/etc/hosts
-echo "127.0.1.1 archlinux.localdomain archlinux" >> /mnt/etc/hosts
-
-curl https://raw.githubusercontent.com/VijayakumarRavi/Dotfiles/main/Arch-Scripts/chroot.sh > /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
+curl https://raw.githubusercontent.com/VijayakumarRavi/Dotfiles/main/Arch-Scripts/chroot.sh > /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh && umount -a && swapoff /dev/sda2 && lsblk ;
 
--- Arch-Scripts/chroot.sh
+++ Arch-Scripts/chroot.sh
@@ -1,55 +1,26 @@
 #!/bin/bash
 
-echo root:vijay | chpasswd
+echo root:vijay | chpasswd ;
 
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-timedatectl set-ntp true
-hwclock --systohc
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime && timedatectl set-ntp true && hwclock --systohc ;
 
 
 
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "en_US ISO-8859-1" >> /etc/locale.gen
-locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf ;
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen ;
+echo "en_US ISO-8859-1" >> /etc/locale.gen ;
+locale-gen ;
 
 # pacman --noconfirm --needed -S networkmanager
-systemctl enable NetworkManager
-sleep 5
-systemctl start NetworkManager
-sleep 5
-systemctl enable gdm
-sleep 5
-systemctl enable bluetooth
-sleep 5
-systemctl enable cups.service
-sleep 5
-systemctl enable sshd
-sleep 5
-systemctl enable avahi-daemon
-sleep 5
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-sleep 5
-systemctl enable reflector.timer
-sleep 5
-systemctl enable fstrim.timer
-sleep 5
-systemctl enable libvirtd
-sleep 5
-systemctl enable firewalld
-sleep 5
-systemctl enable acpid
-sleep 5
+systemctl enable NetworkManager && systemctl start NetworkManager && systemctl enable gdm && systemctl enable bluetooth && systemctl enable cups.service && systemctl enable sshd && systemctl enable avahi-daemon && systemctl enable tlp && systemctl enable reflector.timer && systemctl enable fstrim.timer && systemctl enable libvirtd && systemctl enable firewalld && systemctl enable acpid ;
 
 # pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
-grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg ;
 
 
-useradd -m vijay
-echo vijay:vijay | chpasswd
-usermod -aG libvirt vijay
+useradd -m vijay && echo vijay:vijay | chpasswd && usermod -aG libvirt vijay ;
 
-echo "vijay ALL=(ALL) ALL" >> /etc/sudoers.d/vijay
+echo "vijay ALL=(ALL) ALL" >> /etc/sudoers.d/vijay ;
 
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m" ;
 
