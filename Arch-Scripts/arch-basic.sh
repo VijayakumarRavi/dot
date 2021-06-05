12e1535aa4bb3ffc0ce32a7f43fe83b8690633d0
Vijayakumar Ravi <vijayravi2998@gmail.com>
2021-06-05 11:07:24 +0530

    Minior change

--- Arch-Scripts/arch-basic.sh
+++ Arch-Scripts/arch-basic.sh
@@ -64,7 +64,7 @@ mountfs() {
 
 install-pkgs() {
 	pacman -Sy --noconfirm archlinux-keyring
-	pacstrap /mnt base base-devel linux linux-headers linux-firmware xf86-video-nouveau git neovim intel-ucode curl htop neofetch python-pip gawk grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools avahi gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync acpi acpi_call tlp dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font cups reflector polkit udisks2 pulseaudio-bluetooth
+	pacstrap /mnt base base-devel linux linux-headers linux-firmware xf86-video-nouveau git neovim intel-ucode curl htop neofetch python-pip gawk grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools avahi gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync acpi acpi_call tlp dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font cups reflector polkit udisks2 pulseaudio-bluetooth npm
 	genfstab -U /mnt >> /mnt/etc/fstab ;
 	cat /mnt/etc/fstab ;
 	sleep 20
@@ -129,7 +129,90 @@ starting-service
 config-users
 grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
 sleep 10
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+#printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+printf "\e[1;32m*********Dotfiles Scripts Started**********/n\e[0m"
+HOME=/home/vijay
+dotfiles_dir=/home/vijay/Git/Dotfiles
+BPurple='\e[1;35m'
+NC="\e[m"
+create-dirs() {
+if [[ ! -d $HOME/Git ]]; then
+	mkdir -p $HOME/Git ;
+	echo "Git folder is created" ;
+else
+	echo "Git is already exist" ;
+fi
+
+if [[ ! -d $HOME/.config ]]; then
+	mkdir $HOME/.config ;
+	echo ".config folder is created" ;
+else
+	echo ".config already exist" ;
+fi
+}
+
+del-existing-files() {
+	rm -rvif $HOME/.vim > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.vimrc > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.bashrc > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.tmux > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.tmux.conf > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.zsh_prompt > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.zshrc > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.gitconfig > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.antigen > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.antigen.zsh > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.psqlrc > /dev/null 2>&1 ;
+	rm -rvfi $HOME/.tigrc > /dev/null 2>&1 ;
+}
+
+cloneing-repos(){
+	cd $HOME/Git/
+	git clone --recursive https://github.com/VijayakumarRavi/Dotfiles.git ;
+	git clone --recursive https://github.com/akinomyoga/ble.sh.git ;
+}
+
+link-files() {
+	ln -svf $dotfiles_dir/pacman.conf /etc/ ;
+	ln -svf $dotfiles_dir/nvim $HOME/.config/ ;
+	ln -svf $dotfiles_dir/htop $HOME/.config/ ;
+	ln -svf $dotfiles_dir/neofetch $HOME/.config/ ;
+	ln -svf $dotfiles_dir/zsh/ $HOME/.config/ ;
+	ln -svf $dotfiles_dir/tmux/ $HOME/.config/ ;
+	ln -svf $dotfiles_dir/.bashrc $HOME/.bashrc ;
+	ln -svf $dotfiles_dir/.conkyrc $HOME/.conkyrc ;
+	ln -svf $dotfiles_dir/.gitconfig $HOME/.gitconfig ;
+	ln -svf $dotfiles_dir/zsh/.zshrc $HOME/.zshrc ;
+}
+
+repo-install() {
+	cd $HOME/Git
+	make -C ble.sh install PREFIX=$HOME/.local ;
+}
+
+others() {
+	cd $HOME/Git
+	curl https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/fm6000.pl --output fm6000 && chmod +x fm6000 && sudo mv fm6000 /usr/bin/ ;
+	npm install -g neovim ;
+	npm install --save nord ;
+	pip3 install pynvim ;
+	curl -L https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -o $HOME/.git-completion.bash ;
+	curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o $HOME/.git-prompt.sh ;
+}
+
+grub-theme() {
+	cd $dotfiles_dir/themes
+	bash install.sh ;
+}
+
+create-dirs
+del-existing-files
+cloneing-repos
+link-files
+repo-install
+grub-theme
+others
 EOF
 }
 printf "\e[1;32m*********Arch Scripts Started**********\e[0m"
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "\e[1;32m*********Dotfiles Scripts Started**********/n\e[0m"
+# printf "\e[1;32m*********Dotfiles Scripts Started**********/n\e[0m"
 #==============
 # Variables
 #==============
@@ -8,7 +8,7 @@ HOME=/home/vijay
 dotfiles_dir=/home/vijay/Git/Dotfiles
 BPurple='\e[1;35m'
 NC="\e[m"
-
+create-dirs() {
 if [[ ! -d $HOME/Git ]]; then
 	mkdir -p $HOME/Git ;
 	echo "Git folder is created" ;
@@ -22,6 +22,7 @@ if [[ ! -d $HOME/.config ]]; then
 else
 	echo ".config already exist" ;
 fi
+}
 
 del-existing-files() {
 	rm -rvif $HOME/.vim > /dev/null 2>&1 ;
@@ -42,7 +43,6 @@ cloneing-repos(){
 	cd $HOME/Git/
 	git clone --recursive https://github.com/VijayakumarRavi/Dotfiles.git ;
 	git clone --recursive https://github.com/akinomyoga/ble.sh.git ;
-	git clone https://github.com/arcticicestudio/nord-gnome-terminal.git ;
 }
 
 link-files() {
@@ -61,7 +61,6 @@ link-files() {
 repo-install() {
 	cd $HOME/Git
 	make -C ble.sh install PREFIX=$HOME/.local ;
-	bash nord-gnome-terminal/src/nord.sh ;
 }
 
 others() {
@@ -70,9 +69,6 @@ others() {
 	npm install -g neovim ;
 	npm install --save nord ;
 	pip3 install pynvim ;
-	curl -L 'https://raw.githubusercontent.com/VijayakumarRavi/Wallpapers/main/From%20reddit.jpeg' --output /usr/share/backgrounds/gnome/wall.jpg ;
-	gsettings set org.gnome.desktop.background picture-uri '/usr/share/backgrounds/gnome/wall.jpg' ;
-	gsettings set org.gnome.desktop.screensaver picture-uri '/usr/share/backgrounds/gnome/wall.jpg' ;
 	curl -L https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -o $HOME/.git-completion.bash ;
 	curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o $HOME/.git-prompt.sh ;
 }
@@ -82,6 +78,7 @@ grub-theme() {
 	bash install.sh ;
 }
 
+create-dirs
 del-existing-files
 cloneing-repos
 link-files
