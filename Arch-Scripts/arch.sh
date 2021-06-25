f6ba2abafdf6a98372087ddb0d2b5413e67e9595
Vijayakumar Ravi <vijayravi2998@gmail.com>
2021-06-25 07:38:20 +0530

    dotfiles install testing

--- Arch-Scripts/arch.sh
+++ Arch-Scripts/arch.sh
@@ -232,18 +232,85 @@ grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 }
 
-sysboot() {
-	cat <<EOF | arch-chroot /mnt bash
-bootctl --path=/boot install
-echo ' ' > /boot/loader/loader.conf
-echo "default arch-*" >> /boot/loader/loader.conf
-echo "timeout 3" >> /boot/loader/loader.conf
-echo "#console-mode max" >> /boot/loader/loader.conf
-touch /boot/loader/entries/arch.conf
-echo "title 	 Arch Linux" >> /boot/loader/entries/arch.conf
-echo "linux 	 /vmlinuz-linux" >> /boot/loader/entries/arch.conf
-echo "initrd 	 /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-echo "options 	 root=/dev/sda3 rw" >> /boot/loader/entries/arch.conf
+dots() {
+	cat <<EOF | arch-chroot /mnt bash sudo -U vijay
+#!/bin/bash
+
+# printf "\e[1;32m*********Dotfiles Scripts Started**********/n\e[0m"
+#==============
+# Variables
+#==============
+HOME=/home/vijay
+dotfiles_dir=/home/vijay/git/dot/config
+BPurple='\e[1;35m'
+NC="\e[m"
+create-dirs() {
+if [[ ! -d $HOME/git ]]; then
+	mkdir -p $HOME/git ;
+	echo "git folder is created" ;
+else
+	echo "git is already exist" ;
+fi
+
+if [[ ! -d $HOME/.config ]]; then
+	mkdir $HOME/.config ;
+	echo ".config folder is created" ;
+else
+	echo ".config already exist" ;
+fi
+}
+
+cloneing-repos(){
+	# cd $HOME/git/
+	git clone --recursive https://github.com/VijayakumarRavi/dot.git $HOME/git/dot/ ;
+	git clone --recursive https://github.com/akinomyoga/ble.sh.git $HOME/git/ble.sh/ ;
+	make -C $HOME/git/ble.sh install PREFIX=$HOME/.local ;
+}
+
+link-files() {
+	sudo ln -svf $dotfiles_dir/pacman.conf /etc/ ;
+	ln -svf $dotfiles_dir/alacritty $HOME/.config/ ;
+	ln -svf $dotfiles_dir/htop $HOME/.config/ ;
+	ln -svf $dotfiles_dir/neofetch $HOME/.config/ ;
+	ln -svf $dotfiles_dir/i3 $HOME/.config/ ;
+	ln -svf $dotfiles_dir/i3status $HOME/.config/ ;
+	ln -svf $dotfiles_dir/nvim $HOME/.config/ ;
+	ln -svf $dotfiles_dir/pcmanfm $HOME/.config/ ;
+	ln -svf $dotfiles_dir/picom $HOME/.config/ ;
+	ln -svf $dotfiles_dir/polybar $HOME/.config/ ;
+	ln -svf $dotfiles_dir/tmux/ $HOME/.config/ ;
+	ln -svf $dotfiles_dir/transmission/ $HOME/.config/ ;
+	ln -svf $dotfiles_dir/zsh/ $HOME/.config/ ;
+	ln -svf $dotfiles_dir/.bashrc $HOME/.bashrc ;
+	ln -svf $dotfiles_dir/.inputrc $HOME/.inputrc ;
+	ln -svf $dotfiles_dir/.conkyrc $HOME/.conkyrc ;
+	ln -svf $dotfiles_dir/.gitconfig $HOME/.gitconfig ;
+	ln -svf $dotfiles_dir/zsh/.zshrc $HOME/.zshrc ;
+}
+
+others-settings() {
+	cd $HOME/git
+	curl https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/fm6000.pl --output fm6000 && chmod +x fm6000 && sudo mv fm6000 /usr/bin/ ;
+	sudo npm install -g neovim ;
+	sudo npm install --save nord ;
+	pip3 install pynvim ;
+	curl -L https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -o $HOME/.git-completion.bash ;
+	curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o $HOME/.git-prompt.sh ;
+}
+
+grub-theme() {
+	cd $HOME/git/dot/themes
+	sudo bash install.sh ;
+}
+
+create-dirs
+cloneing-repos
+link-files
+grub-theme
+others-settings
+
+echo -e "\n\nApram ena ba!!\nNeeye pathukoo\nEllam adhu edathula vechachi\n"
+
 EOF
 }
 
@@ -270,7 +337,7 @@ de_choose() {
   exec 3>&1
   DE=$(dialog \
     --backtitle "Arch Installation" \
-    --title "Menu" \
+    --title "Select Desktop type" \
     --cancel-label "Exit" \
     --menu "Please select:" $HEIGHT $WIDTH 4 \
     "1" "Gnome" \
@@ -307,9 +374,9 @@ uefi_install() {
 	uefi_makefs
 	install_pkgs
 	chroot_ex
-	#grub_uefi
-	sysboot
+	grub_uefi
 	de_type
+	dots
 	printf "\e[1;35m\n\nUEFI Installation completed \n\e[0m"
 }
 
@@ -320,6 +387,7 @@ mbr_install() {
 	chroot_ex
 	grub_mbr
 	de_type
+	dots
 	printf "\e[1;35m\n\nMBR Installation completed  \e[0m"
 }
 
@@ -332,7 +400,7 @@ main() {
   exec 3>&1
   selection=$(dialog \
     --backtitle "Arch Installation" \
-    --title "Menu" \
+    --title "Select Firmware type" \
     --cancel-label "Exit" \
     --menu "Please select:" $HEIGHT $WIDTH 4 \
     "1" "UEFI" \
