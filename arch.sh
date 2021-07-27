6405a78e7de8a3f912c4fdf561cb88ed7e700522
Vijayakumar Ravi <vijayravi2998@gmail.com>
2021-07-27 14:29:13 +0530

    Unofficial user repositories added

--- arch.sh
+++ arch.sh
@@ -146,7 +146,7 @@ EOF
 install_pkgs() {
 	clear
 	echo "Installing Required packages"
-	pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 3056513887B78AEB && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && pacman-key --recv-key B545E9B7CD906FE3 && pacman-key --lsign-key B545E9B7CD906FE3 && curl -L https://vijayakumarravi.github.io/dot/config/pacman.conf --output /etc/pacman.conf
+	pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 3056513887B78AEB && pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && pacman-key --recv-key B545E9B7CD906FE3 && pacman-key --lsign-key B545E9B7CD906FE3 && curl -L https://vijayakumarravi.github.io/dot/config/pacman.conf --output /etc/pacman.conf
 
 	pacman -Sy --noconfirm archlinux-keyring
 	pacstrap /mnt base base-devel linux linux-headers linux-firmware xf86-video-nouveau git neovim intel-ucode curl htop neofetch python-pip gawk grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools avahi gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync acpi acpi_call tlp iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font cups reflector polkit udisks2 pulseaudio-bluetooth npm
