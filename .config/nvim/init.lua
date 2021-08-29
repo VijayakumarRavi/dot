5ecbc2984e1ced9d1ce7056b934e37f37cf3b61f
Vijayakumar Ravi <vijayravi2998@gmail.com>
2021-08-30 00:27:09 +0530

    Upgradeing to lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,7 +4,7 @@
 vim.g.mapleader = ' '
 -------------------- HELPERS -------------------------------
 local api, cmd, fn, g = vim.api, vim.cmd, vim.fn, vim.g
-local opt, wo = vim.opt, vim.wo
+local opt, wo, op = vim.opt, vim.wo, vim.o
 local fmt = string.format
 
 local function map(mode, lhs, rhs, opts)
@@ -14,36 +14,43 @@ local function map(mode, lhs, rhs, opts)
 end
 
 -------------------- PLUGINS -------------------------------
+
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+end
+
 cmd 'packadd paq-nvim'
 local paq = require('paq-nvim').paq
-paq {'airblade/vim-rooter'}
-paq {'hrsh7th/nvim-compe'}
-paq {'joshdick/onedark.vim'}
-paq {'junegunn/fzf'}
-paq {'junegunn/fzf.vim'}
-paq {'justinmk/vim-dirvish'}
-paq {'lervag/vimtex'}
-paq {'lewis6991/gitsigns.nvim'}
-paq {'lukas-reineke/indent-blankline.nvim'}
-paq {'machakann/vim-sandwich'}
-paq {'neovim/nvim-lspconfig'}
-paq {'nvim-lua/plenary.nvim'}
-paq {'nvim-treesitter/nvim-treesitter'}
-paq {'nvim-treesitter/nvim-treesitter-textobjects'}
-paq {'ojroques/nvim-bufbar'}
-paq {'ojroques/nvim-bufdel'}
-paq {'ojroques/nvim-buildme'}
-paq {'ojroques/nvim-hardline'}
-paq {'ojroques/nvim-lspfuzzy'}
-paq {'ojroques/vim-oscyank'}
-paq {'savq/paq-nvim', opt = true}
-paq {'terrortylor/nvim-comment'}
-paq {'tpope/vim-fugitive'}
-paq {'tpope/vim-unimpaired'}
-paq {'nvim-telescope/telescope.nvim'}
-paq {'nvim-lua/plenary.nvim'}
-paq {'kyazdani42/nvim-web-devicons'}
+paq {'savq/paq-nvim', opt = true}                             -- Neovim package manager written in Lua.
+paq {'airblade/vim-rooter'}                                   -- Changes the working directory to the project root when you open a file or directory.
+paq {'joshdick/onedark.vim'}                                  -- A dark Vim/Neovim color scheme.
+paq {'lewis6991/gitsigns.nvim'}                               -- Git decorations implemented purely in lua/teal.
+paq {'lukas-reineke/indent-blankline.nvim'}                   -- This plugin adds indentation guides to all lines (including empty lines).
 
+paq {'ojroques/nvim-bufbar'}                                  -- A simple and very light bufferline for Neovim written in Lua.
+paq {'ojroques/nvim-bufdel'}                                  -- A very small Neovim plugin to improve the deletion of buffers.
+paq {'ojroques/nvim-buildme'}                                 -- A Neovim plugin to build or run a project using the built-in terminal.
+paq {'ojroques/nvim-hardline'}                                -- A statusline / bufferline for Neovim written in Lua.
+paq {'ojroques/vim-oscyank'}                                  -- A Vim / Neovim plugin to copy text to the system clipboard from anywhere using the ANSI OSC52 sequence.
+paq {'ojroques/nvim-lspfuzzy'}
+
+paq {'tpope/vim-fugitive'}                                    -- A Git wrapper.
+paq {'tpope/vim-unimpaired'}                                  -- This plugin provides several pairs of bracket maps. Reff -- https://github.com/tpope/vim-unimpaired/blob/master/doc/unimpaired.txt 
+paq {'nvim-telescope/telescope.nvim'}                         -- Its a highly extendable fuzzy finder over lists.
+paq {'nvim-lua/plenary.nvim'}                                 -- A Lua module for asynchronous programming using coroutines {Telescope dependency}.
+paq {'kyazdani42/nvim-web-devicons'}                          -- Icons for Telescope.
+paq {'junegunn/fzf'}                                          -- Fzf is a general-purpose command-line fuzzy finder.
+paq {'junegunn/fzf.vim'}                                      -- Fzf vim version.
+paq {'justinmk/vim-dirvish'}                                  -- Path navigator designed to work with Vim's built-in mechanisms and complementary plugins.
+paq {'christoomey/vim-tmux-navigator'}                        -- For Tmux pane switching.
+
+paq {'neovim/nvim-lspconfig'}                                 -- Lsp Client for nvim.
+paq {'hrsh7th/nvim-compe'}                                    -- Auto completion plugin for nvim.
+paq {'kabouzeid/nvim-lspinstall'}                             -- Automatic lsp Install.
+paq {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}    -- Tree-sitter is a parser generator tool and an incremental parsing library.
+paq {'nvim-treesitter/nvim-treesitter-textobjects'}           -- Create your own textobjects using tree-sitter queries.
 
 -------------------- PLUGIN SETUP --------------------------
 -- fzf & fzf.vim
@@ -53,6 +60,7 @@ map('n', '<leader>f', '<cmd>Files<CR>')
 map('n', '<leader>;', '<cmd>History:<CR>')
 map('n', '<leader>r', '<cmd>Rg<CR>')
 map('n', 's', '<cmd>Buffers<CR>')
+
 -- gitsigns.nvim
 require('gitsigns').setup {
   signs = {
@@ -61,46 +69,40 @@ require('gitsigns').setup {
     delete = {text = '-'}, topdelete = {text = '-'}, changedelete = {text = '≃'},
   },
 }
+
 -- indent-blankline.nvim
 g['indent_blankline_char'] = '┊'
 g['indent_blankline_buftype_exclude'] = {'terminal'}
 g['indent_blankline_filetype_exclude'] = {'fugitive', 'fzf', 'help', 'man'}
+
 -- nvim-bufbar
-require('bufbar').setup {show_bufname = 'visible', show_flags = false}
+require('bufbar').setup {show_bufname = 'visible', show_tabs = true, show_flags = true}
+
 -- nvim-bufdel
 require('bufdel').setup {next = 'alternate', quit = false}
 map('n', '<leader>w', '<cmd>BufDel<CR>')
+
 -- nvim-buildme
-map('n', '<leader>bb', '<cmd>BuildMe<CR>')
+map('n', 'bb', '<cmd>BuildMe<CR>')
 map('n', '<leader>be', '<cmd>BuildMeEdit<CR>')
 map('n', '<leader>bs', '<cmd>BuildMeStop<CR>')
--- nvim-comment
-require('nvim_comment').setup {}
--- nvim-compe
-require('compe').setup {
-  min_length = 2,
-  preselect = 'disable',
-  max_abbr_width = 80, max_kind_width = 40, max_menu_width = 40,
-  source = {buffer = true, path = true, nvim_lsp = true, omni = {filetypes = {'tex'}}},
-}
 
 -- nvim-hardline
 require('hardline').setup {}
+
 -- nvim-lspfuzzy
 require('lspfuzzy').setup {}
+
 -- vim-dirvish
 g['dirvish_mode'] = [[:sort ,^.*[\/],]]
 map('', '<leader>d', ':Shdo ')
+
 -- vim-fugitive & git
 local log = [[\%C(yellow)\%h\%Cred\%d \%Creset\%s \%Cgreen(\%ar) \%Cblue\%an\%Creset]]
 map('n', '<leader>g<space>', ':Git ')
 map('n', '<leader>gd', '<cmd>Gvdiffsplit<CR>')
 map('n', '<leader>gg', '<cmd>Git<CR>')
 map('n', '<leader>gl', fmt('<cmd>term git log --graph --all --format="%s"<CR><cmd>start<CR>', log))
--- vim-sandwich
-cmd 'runtime macros/sandwich/keymap/surround.vim'
--- vimtex
-g['vimtex_quickfix_mode'] = false
 
 -------------------- OPTIONS -------------------------------
 local indent, width = 4, 180
@@ -132,13 +134,15 @@ opt.textwidth = width               -- Maximum width of text
 opt.updatetime = 100                -- Delay before swap file is saved
 opt.wildmode = {'list', 'longest'}  -- Command-line completion mode
 opt.wrap = false                    -- Disable line wrap
-
+opt.undofile = true                 -- Enable undo after reopening
+opt.undodir = '/tmp'                -- Location to save the undo file
+opt.spelllang='en'                    -- Spell cleck languages
 cmd 'colorscheme onedark'
 cmd 'set nohlsearch'
--- cmd 'set mouse=a'
+--cmd 'set mouse=a'
 
 -------------------- MAPPINGS ------------------------------
-map('', '<leader>c', '"+y')
+map('', 'y', '"+y')
 map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
 map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
 map('i', 'jj', '<ESC>')
@@ -163,22 +167,75 @@ map('t', 'jj', '<ESC>', {noremap = false})
 map('v', '<leader>s', ':s//gcI<Left><Left><Left><Left>')
 
 -------------------- LSP -----------------------------------
-for ls, cfg in pairs({
-  bashls = {}, gopls = {}, ccls = {}, jsonls = {}, pylsp = {},
-}) do require('lspconfig')[ls].setup(cfg) end
-map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
-map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
-map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
-map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
-map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+require('lspinstall').setup() -- important
+
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    require'lspconfig'[server].setup{}
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
+
+map('i', '<A-,>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('i', '<A-;>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('i', '<A-a>', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('i', '<A-d>', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('i', '<A-f>', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('i', '<A-h>', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('i', '<A-m>', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('i', '<A-r>', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('i', '<A-s>', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+
+
+
+-------------------- Compe Setup ---------------------------
+require('compe').setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  resolve_timeout = 800;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = {
+    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  };
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    Spell = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+    ultisnips = true;
+    luasnip = true;
+  };
+}
 
 -------------------- TREE-SITTER ---------------------------
 require('nvim-treesitter.configs').setup {
-  ensure_installed = 'maintained',
+  ensure_installed = 'all',
   highlight = {enable = true},
   textobjects = {
     select = {
@@ -240,6 +297,13 @@ vim.tbl_map(function(c) cmd(fmt('autocmd %s', c)) end, {
 
 ------ My Custom Testing lua part ------
 
+------ Remember cursor position --------
+cmd ([[
+" Go to the last cursor location when a file is opened, unless this is a
+" git commit (in which case it's annoying)
+autocmd BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") && &filetype != "gitcommit" | execute("normal `\"") | endif
+]])
+
 ------ commenting_blocks_of_code -------
 cmd ([[
 augroup commenting_blocks_of_code
@@ -261,10 +325,9 @@ noremap <silent> ,cu :<C-B>silent <C-E>s/^\V<C-R>=escape(b:comment_leader,'\/')<
 map('v', 'J', ":m '>+1<CR>gv=gv")
 map('v', 'K', ":m '<-2<CR>gv=gv")
 
-cmd (":autocmd TermOpen * setlocal statusline=%{b:term_title}")
-
 ------ Telescope ----
-map('n', '<leader>f', '<cmd>Telescope find_files<cr>')
+map('n', '<leader>f', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden',  '-g', '!.git' }})<cr>", default_opts)
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 map('n', '<leader>g', '<cmd>Telescope live_grep<cr>')
 map('n', '<leader>b', '<cmd>Telescope buffers<cr>')
 map('n', '<leader>h', '<cmd>Telescope help_tags<cr>')
@@ -278,25 +341,17 @@ require("telescope").setup {
       override_generic_sorter = false,
       override_file_sorter = true,
       case_mode = "smart_case", -- this is default
+      hidden = true
     },
   },
   defaults = {
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-      "--hidden",
-    },
+    vimgrep_arguments = { "rg", "--no-heading", "--hidden" },
     prompt_prefix = "❯ ",
     selection_caret = "❯ ",
     sorting_strategy = "ascending",
     color_devicons = true,
     layout_config = {
-      prompt_position = "bottom",
+      prompt_position = "top",
       horizontal = {
         width_padding = 0.04,
         height_padding = 0.1,
@@ -308,7 +363,16 @@ require("telescope").setup {
         preview_height = 0.5,
       },
     },
-    mappings = { n = { ["<Del>"] = actions.close } },
+    mappings = { i = { ["<Del>"] = actions.close } },
   },
 }
+
+
+----- Esc & Closing actions -----
+map('i', '<F1>', '<Esc>')
+map('i', '<C-c>', '<Esc>')
+map('n', '<C-c>', ':wq<CR>')
+map('n', '<C-z>', ':wa<CR>')
+map('n', '<leader> ', ':so %<CR>')
+
 
