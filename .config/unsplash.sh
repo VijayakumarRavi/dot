ecfc4b24c3abb98f12fdcab2e90b24c34d5fcbcd
Vijayakumar Ravi <vijayravi2998@gmail.com>
2021-10-23 23:41:31 +0530

    Clean Ups

--- .config/unsplash.sh
+++ .config/unsplash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [[ $(nmcli -t -f GENERAL.METERED dev show wlp4s0) = 'GENERAL.METERED:no (guessed)' ]]; then
+if [[ $(nmcli -t -f GENERAL.METERED dev show enp0s3) = 'GENERAL.METERED:no (guessed)' ]]; then
         wget -O /var/tmp/wallpaper.jpg https://source.unsplash.com/1600x900/?water,snow && feh --bg-fill /var/tmp/wallpaper.jpg
 fi
